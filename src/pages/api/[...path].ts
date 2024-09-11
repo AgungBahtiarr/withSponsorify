7d61100d00e1852b862a515555546619aef5edb2
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-09-11 07:09:50 +0700

    add drizzle example

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -1,12 +1,17 @@
 import { Hono } from "hono";
 import type { APIRoute } from "astro";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { user } from "../../schema";
+import postgres from "postgres";
+const client = postgres(import.meta.env.DATABASE_URL);
+const db = drizzle(client);
+
+const users = await db.select().from(user);
 
 const app = new Hono().basePath("/api");
 
 app.get("/hello", async (c) => {
-  return c.json({
-    Hello: "Hello From Hono",
-  });
+  return c.json(users);
 });
 
 export const ALL: APIRoute = async ({ request }) => {
