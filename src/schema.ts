0294b8359eb16d73e898a41ac832b1f773b4d01f
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-09-11 14:09:29 +0700

    add event table

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,24 @@
-import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { serial, text, timestamp, pgTable, date } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: serial("id"),
   name: text("name"),
   email: text("email"),
   password: text("password"),
-  role: text("role").$type<"admin" | "customer">(),
+  role: text("role").$type<"event_organizer" | "sponsor" | "admin">(),
+  createdAt: timestamp("created_at"),
+  updatedAt: timestamp("updated_at"),
+});
+
+export const events = pgTable("events", {
+  id: serial("id"),
+  name: text("name"),
+  description: text("description"),
+  location: text("location"),
+  proposal: text("proposal"),
+  startDate: date("start_date"),
+  endDate: date("end_date"),
+  userId: serial("user_id"),
   createdAt: timestamp("created_at"),
   updatedAt: timestamp("updated_at"),
 });
